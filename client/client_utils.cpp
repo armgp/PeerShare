15b8a05b66509b0c7a4f82a60f0752ef34cecf29
Abhiram G P <49094298+armgp@users.noreply.github.com>
2022-10-23 18:55:13 +0530

    Update client_utils.cpp

--- client/client_utils.cpp
+++ client/client_utils.cpp
@@ -607,6 +607,11 @@ void client(string req, string ip, int port){
         else{
 
             std::cout << "**********[<USERS WITH FILE>: " << response << "]**********\n";
+            
+            if(response.size() == 0){
+                std::cout<<"**********[<NO USERS WITH THE FILE FOUND>]**********\n";
+                return;
+            }
 
             vector<string> usersInfo = divideStringByChar(response, ' ');
             unordered_map<string, pair<string, int>> userDetails;
